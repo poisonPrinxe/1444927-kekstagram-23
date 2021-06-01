488a9c3e5e986d1b42523c3861dc0b1256ac6927
poisonPrinxe <77621150+poisonPrinxe@users.noreply.github.com>
2021-06-01 16:20:18 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 function getRandomInRange(min, max) {
   min = Math.ceil(min);
   max = Math.floor(max);
+  if (min === max) {
+    return min;
+  }
   return Math.floor(Math.random() * Math.abs(max - min + 1)) + min;
 }
 
