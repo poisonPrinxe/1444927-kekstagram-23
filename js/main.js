80c8c83e8f35f854c06bf66dd05811b1bfd6b0ab
poisonPrinxe <77621150+poisonPrinxe@users.noreply.github.com>
2021-06-09 11:31:16 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -40,17 +40,17 @@ function getPhotosAndComments(amount, descriptionsInFunction) {
     'Ян',
     'Лиза',
   ];
-  for (var i = 0; i < amount; i++) {
-    photosInFunction[i] = {
-      id: i + 1,
-      url: 'photos/' + (i + 1).toString() + '.jpg',
-      description: descriptionsInFunction[i],
+  for (let counter1 = 0; counter1 < amount; counter1++) {
+    photosInFunction[counter1] = {
+      id: counter1 + 1,
+      url: 'photos/' + (counter1 + 1).toString() + '.jpg',
+      description: descriptionsInFunction[counter1],
       likes: getRandomInRange(15, 200),
       comments: [],
     };
-    for (var j = 0; j < getRandomInRange(3, 8); j++) {
-      photosInFunction[i].comments[j] = {
-        id: (i + 1) * 10 + j + 1,
+    for (var counter2 = 0; counter2 < getRandomInRange(3, 8); counter2++) {
+      photosInFunction[counter1].comments[counter2] = {
+        id: (counter1 + 1) * 10 + counter2 + 1,
         avatar: 'img/avatar-' + getRandomInRange(1, 6).toString() + '.svg',
         message: possibleComments[getRandomInRange(0, 5)],
         name: possibleNames[getRandomInRange(0, 7)],
