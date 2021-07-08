3daa8321e7094b23a6ca86fe4691d4b97a374763
poisonPrinxe <77621150+poisonPrinxe@users.noreply.github.com>
2021-07-08 17:25:06 +0300

    Add files via upload

--- js/image-upload.js
+++ js/image-upload.js
@@ -1,8 +1,9 @@
-const upload = document.querySelector('#upload-file');
+﻿const upload = document.querySelector('#upload-file');
 const form = document.querySelector('.img-upload__overlay');
 const closeButton = form.querySelector('.img-upload__cancel');
 const descriptionInput = form.querySelector('.text__description');
 const hashtagsInput = form.querySelector('.text__hashtags');
+const HASHTAG_PATTERN = /^(#[a-z0-9]{1,19} ){0,4}(#[a-z0-9]{1,19})$/gmi;
 
 upload.addEventListener('change', () => {
   form.classList.remove('hidden');
@@ -13,10 +14,6 @@ closeButton.addEventListener('click', () => {
   upload.value = '';
   form.classList.add('hidden');
   document.querySelector('body').classList.remove('modal-open');
-});
-
-descriptionInput.addEventListener('keydown', () => {
-  event.stopPropagation();
 });
 
 window.addEventListener('keydown', (event) => {
@@ -30,9 +27,20 @@ window.addEventListener('keydown', (event) => {
       break;
   }
   event.preventDefault();
-}, true);
+});
+
+descriptionInput.addEventListener('keydown', () => {
+  event.stopPropagation();
+});
+
+hashtagsInput.addEventListener('keydown', () => {
+  event.stopPropagation();
+});
 
 hashtagsInput.addEventListener('change', () => {
-  const hashtags = hashtagsInput.split(' ');
-  console.log(hashtags);
+  if (hashtagsInput.validity.patternMismatch) {
+    hashtagsInput.setCustomValidity('Хештеги должны быть указаны в следующем формате:\n- От 0 до 5 хештегов\n- Каждый хештег начинается с #\n- Длина хештега от 2 до 20 символов, считая #\n- В тексте хештега могут быть только латинские буквы и цифры\n- Хештеги должны быть разделены пробелами');
+  } else {
+    hashtagsInput.setCustomValidity('');
+  }
 });
