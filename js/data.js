04b4be6dad9266baa5b97bc132874b5d2c6a9211
poisonPrinxe <77621150+poisonPrinxe@users.noreply.github.com>
2021-07-13 20:27:31 +0300

    Add files via upload

--- js/data.js
+++ js/data.js
@@ -20,8 +20,6 @@ const POSSIBLE_NAMES = [
   'Лиза',
 ];
 
-const number = getRandomInRange(3, 8);
-
 function getPhotosAndComments(amount, descriptionsInFunction) {
   const photosInFunction = [];
   for (let counter1 = 0; counter1 < amount; counter1++) {
@@ -32,7 +30,7 @@ function getPhotosAndComments(amount, descriptionsInFunction) {
       likes: getRandomInRange(15, 200),
       comments: [],
     };
-    for (let counter2 = 0; counter2 < number; counter2++) {
+    for (let counter2 = 0; counter2 < getRandomInRange(3, 18); counter2++) {
       photosInFunction[counter1].comments[counter2] = {
         id: (counter1 + 1) * 10 + counter2 + 1,
         avatar: `img/avatar-${getRandomInRange(1, 6)}.svg`,
--- js/miniatures.js
+++ js/miniatures.js
@@ -1,16 +1,21 @@
 function putPicturesOnWebsite (pictures) {
+
   const imageTemplate = document.querySelector('#picture');
   const picturesBlock = document.querySelector('.pictures');
   const picturesOnWebsite = [];
   const fragment = document.createDocumentFragment();
+
   for (let counter = 0; counter < pictures.length; counter++) {
+
     picturesOnWebsite[counter] = imageTemplate.content.cloneNode(true);
     picturesOnWebsite[counter].querySelector('.picture').id = counter;
     picturesOnWebsite[counter].querySelector('.picture__img').src = pictures[counter].url;
     picturesOnWebsite[counter].querySelector('.picture__img').alt = pictures[counter].description;
     picturesOnWebsite[counter].querySelector('.picture__comments').textContent = pictures[counter].comments.length;
     picturesOnWebsite[counter].querySelector('.picture__likes').textContent = pictures[counter].likes;
+
     picturesOnWebsite[counter].querySelector('.picture').addEventListener('click', () => {
+
       const bigPicture = document.querySelector('.big-picture');
       bigPicture.querySelector('img').src = pictures[counter].url;
       bigPicture.querySelector('.likes-count').textContent = pictures[counter].likes;
@@ -19,18 +24,43 @@ function putPicturesOnWebsite (pictures) {
       const commentTemplate = bigPicture.querySelector('.social__comment').cloneNode(true);
       const comments = [];
       bigPicture.querySelector('.social__comments').innerHTML = '';
-      for (let counterSecond = 0; counterSecond < pictures[counter].comments.length; counterSecond++) {
-        comments[counterSecond] = commentTemplate.cloneNode(true);
-        comments[counterSecond].querySelector('img').src = pictures[counter].comments[counterSecond].avatar;
-        comments[counterSecond].querySelector('img').alt = pictures[counter].comments[counterSecond].name;
-        comments[counterSecond].querySelector('.social__text').textContent = pictures[counter].comments[counterSecond].message;
-        bigPicture.querySelector('.social__comments').appendChild(comments[counterSecond]);
+      const commentGroupsAmount = Math.ceil(pictures[counter].comments.length / 5);
+      const commentGroups = [];
+
+      for (let counterCommentGroups = 0; counterCommentGroups < commentGroupsAmount; counterCommentGroups++) {
+        commentGroups[counterCommentGroups] = pictures[counter].comments.slice(counterCommentGroups * 5, (counterCommentGroups + 1) * 5);
       }
+      let commentGroupCurrent = 0;
+
+      function loadComments () {
+        for (let counterSecond = 0; counterSecond < commentGroups[commentGroupCurrent].length; counterSecond++) {
+          comments[counterSecond] = commentTemplate.cloneNode(true);
+          comments[counterSecond].querySelector('img').src = commentGroups[commentGroupCurrent][counterSecond].avatar;
+          comments[counterSecond].querySelector('img').alt = commentGroups[commentGroupCurrent][counterSecond].name;
+          comments[counterSecond].querySelector('.social__text').textContent = commentGroups[commentGroupCurrent][counterSecond].message;
+          bigPicture.querySelector('.social__comments').appendChild(comments[counterSecond]);
+        }
+        if (commentGroupCurrent + 1 === commentGroups.length) {
+          bigPicture.querySelector('.comments-loader').classList.add('hidden');
+        }
+      }
+
+     loadComments();
+     commentGroupCurrent++;
+
+     bigPicture.querySelector('.comments-loader').addEventListener('click', () => {
+       loadComments();
+       commentGroupCurrent++;
+     });
+
       document.body.classList.add('modal-open');
+
       bigPicture.querySelector('.cancel').addEventListener('click', () => {
         bigPicture.classList.add('hidden');
         document.body.classList.remove('modal-open');
+        bigPicture.querySelector('.comments-loader').classList.remove('hidden');
       });
+
       window.addEventListener('keydown', (event) => {
         if (!bigPicture.classList.contains('hidden')) {
           if (event.defaultPrevented) {
@@ -40,19 +70,23 @@ function putPicturesOnWebsite (pictures) {
             case 'Escape':
               bigPicture.classList.add('hidden');
               document.body.classList.remove('modal-open');
+              bigPicture.querySelector('.comments-loader').classList.remove('hidden');
               break;
           }
           event.preventDefault();
         }
       });
+
       bigPicture.classList.remove('hidden');
-      bigPicture.querySelector('.social__comment-count').classList.add('hidden');
-      bigPicture.querySelector('.comments-loader').classList.add('hidden');
+
     });
 
     fragment.appendChild(picturesOnWebsite[counter]);
+
   }
+
   picturesBlock.appendChild(fragment);
+
 }
 
 export {putPicturesOnWebsite};
