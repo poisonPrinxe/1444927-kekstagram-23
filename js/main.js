84f1e3f1e4702115db574947216c46c785e504e1
poisonPrinxe <77621150+poisonPrinxe@users.noreply.github.com>
2021-07-20 12:21:53 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
 ﻿import {getRandomInRange, checkStringLength} from './utils.js';
-import {getPhotosAndComments} from './data.js';
-import {DESCRIPTIONS} from './constants.js';
 import {putPicturesOnWebsite} from './miniatures.js';
 import './image-upload.js';
 import './getting-server-data.js';
@@ -8,7 +6,3 @@ import './getting-server-data.js';
 checkStringLength('hello, world!', getRandomInRange(0, 140));
 
 // this line was added to surpass the "unused variables" error
-
-// const pictures = getPhotosAndComments(25, DESCRIPTIONS);
-
-// putPicturesOnWebsite(pictures);
