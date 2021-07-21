6759dbaf4fc04c8e0e630ccae7ab133cb8d26729
poisonPrinxe <77621150+poisonPrinxe@users.noreply.github.com>
2021-07-21 21:43:28 +0300

    Update throttle.js

--- js/utils/throttle.js
+++ js/utils/throttle.js
@@ -10,7 +10,7 @@ function throttle (callback, delayBetweenFrames) {
     // Получаем текущую дату в миллисекундах,
     // чтобы можно было в дальнейшем
     // вычислять разницу между кадрами
-    let now = new Date();
+    const now = new Date();
 
     // Если время между кадрами больше задержки,
     // вызываем наш колбэк и перезаписываем lastTime
@@ -18,7 +18,7 @@ function throttle (callback, delayBetweenFrames) {
     if (now - lastTime >= delayBetweenFrames) {
       callback.apply(this, rest)
       lastTime = now;
-    }
+    };
   };
 }
 
